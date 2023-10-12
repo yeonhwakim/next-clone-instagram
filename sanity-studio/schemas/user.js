e33252042fb8f40d8af5d2a18758f3b81ee0cee1
yeonhwa <yeonhwa6717@gmail.com>
2023-10-12 22:07:08 +0900

    Add user schema

---
+++ sanity-studio/schemas/user.js
@@ -0,0 +1,32 @@
+export default {
+  title: 'User',
+  name: 'user',
+  type: 'document',
+  fields: [
+    {title: 'Username', name: 'username', type: 'string'},
+    {title: 'Name', name: 'name', type: 'string'},
+    {title: 'Email', name: 'email', type: 'string'},
+    {title: 'Image', name: 'image', type: 'string'},
+    {
+      title: 'Following',
+      name: 'following',
+      type: 'array',
+      of: {tyep: 'reference', to: {type: 'User'}},
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Follower',
+      name: 'follower',
+      type: 'array',
+      of: {tyep: 'reference', to: {type: 'User'}},
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Bookmarks',
+      name: 'bookmarks',
+      type: 'array',
+      of: {tyep: 'reference', to: {type: 'Post'}},
+      validation: (Rule) => Rule.unique(),
+    },
+  ],
+}
