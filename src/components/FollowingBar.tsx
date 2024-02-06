aa414a4f8cc26784860a3c75ac2c8a7d6918c2b4
yeonhwa <yeonhwa6717@gmail.com>
2024-02-06 21:23:49 +0900

    Implement getUserBuusername service

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -3,7 +3,7 @@
 import useSWR from "swr";
 
 export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR("/api/hello");
+  const { data, isLoading, error } = useSWR("/api/me");
   console.log(data);
   // 1. 클라이언트 컴포넌트에서 백엔드에게 api/me 사용자의 정보를 얻어옴.
   // 2. 백엔드에서는 현재 로그인된 사용자의 세션 정보를 이용해서
